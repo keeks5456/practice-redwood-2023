54197ac290fd91448312bff869fbc216841a1aa0
Akiko Green <akikogreen08@gmail.com>
2023-06-15 12:33:56 -0700

    generate layout, link to home & about

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,12 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-      <header>
-        <h1>Redwood Blog Home</h1>
-        <p>Hello! this is my first generated page! this is so dope!</p>
-        <p>
-          Check out my about page here: `<Link to={routes.about()}>About</Link>`
-        </p>
-      </header>
+      <h1>Redwood Blog Home</h1>
+      <p>Hello! this is my first generated page! this is so dope!</p>
+
       <main>Home</main>
     </>
   )
