acf67741ba5a665a8063f71e0c150c895fb1d57b
Akiko Green <akikogreen08@gmail.com>
2023-06-15 12:39:01 -0700

    clean up layouts

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -6,8 +6,6 @@ const HomePage = () => {
       <MetaTags title="Home" description="Home page" />
       <h1>Redwood Blog Home</h1>
       <p>Hello! this is my first generated page! this is so dope!</p>
-
-      <main>Home</main>
     </>
   )
 }
