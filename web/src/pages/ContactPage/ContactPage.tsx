43ca9c37044147d9803e8aa1bb1593184c59a358
Akiko Green <akikogreen08@gmail.com>
2023-06-17 19:18:24 -0700

    add functionality to form fields

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,6 +1,8 @@
 // import { Link, routes } from '@redwoodjs/router'
 import {
+  FieldError,
   Form,
+  Label,
   TextField,
   TextAreaField,
   Submit,
@@ -23,13 +25,43 @@ const ContactPage = () => {
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form onSubmit={onSubmit}>
-        <label htmlFor="name">Name</label>
-        <TextField name="name" />
-        <label htmlFor="email">Email</label>
-        <TextField name="email" />
-        <label htmlFor="message">Message</label>
-        <TextAreaField name="message" />
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+        <Label name={'name'} errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name={'email'} errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name={'message'} errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
         <Submit>Save</Submit>
       </Form>
     </>
